927c60da8a447a24d0c98c5c15c8c9db329cd441
Aslı Sulutaş <cemree.1402@gmail.com>
2023-03-05 22:36:45 +0300

    add Task

---
+++ src/components/Tasks/Task.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Task.css";
+import MoreIcon from "../../assets/icons/more.svg";
+
+function Task({
+  projectName,
+  taskName,
+  categoryName,
+  tagName,
+  date,
+  priority,
+  userImage,
+  progressValue,
+}) {
+  return (
+    <div className="task-container">
+      <div className="project-name">
+        <input type="checkbox" />
+        <span>{projectName}</span>
+      </div>
+      <div className="task-name">
+        <span>{taskName}</span>
+      </div>
+      <div className="category-name">
+        <span>{categoryName}</span>
+      </div>
+      <div className="tag-name">
+        <span>{tagName}</span>
+      </div>
+      <div className="date-text">
+        <span>{date}</span>
+      </div>
+      <div className="priority-value">
+        <span>{priority}</span>
+      </div>
+      <div className="user-image">
+        <img src={userImage} />
+      </div>
+      <div className="progress-value">
+        <span>{progressValue}</span>
+      </div>
+      <div className="rename-image">
+        <img src={MoreIcon} />
+      </div>
+    </div>
+  );
+}
+
+export default Task;
--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -2,6 +2,40 @@ import React from "react";
 import "./Tasks.css";
 import CopyIcon from "../../assets/icons/copy.svg";
 import TasksHeader from "./TasksHeader";
+import Task from "./Task";
+import UserImage from "../../assets/icons/avatar.svg";
+const TaskData = [
+  {
+    projectName: "Koçtaş UI&UX Maintenance",
+    taskName: "asdasd",
+    categoryName: "adfaf",
+    tagName: "sdfsfsd",
+    date: "12354",
+    priority: "acil",
+    progressValue: "adasfa",
+    userImage: UserImage,
+  },
+  {
+    projectName: "Ford UI&UX Maintenance",
+    taskName: "asdasd",
+    categoryName: "adfaf",
+    tagName: "sdfsfsd",
+    date: "12354",
+    priority: "acil",
+    progressValue: "adasfa",
+    userImage: UserImage,
+  },
+  {
+    projectName: "AI Proje/ AR-GE",
+    taskName: "asdasd",
+    categoryName: "adfaf",
+    tagName: "sdfsfsd",
+    date: "12354",
+    priority: "acil",
+    progressValue: "adasfa",
+    userImage: UserImage,
+  },
+];
 
 function Tasks() {
   return (
@@ -11,6 +45,21 @@ function Tasks() {
         <img src={CopyIcon} />
       </div>
       <TasksHeader />
+      {TaskData.map((item, index) => {
+        return (
+          <Task
+            key={index}
+            projectName={item.projectName}
+            taskName={item.taskName}
+            categoryName={item.categoryName}
+            tagName={item.tagName}
+            date={item.date}
+            priority={item.priority}
+            progressValue={item.progressValue}
+            userImage={item.userImage}
+          />
+        );
+      })}
     </div>
   );
 }
