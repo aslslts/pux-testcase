d3b09b16249912752a8d35c1aac001edd1a73b81
Aslı Sulutaş <cemree.1402@gmail.com>
2023-03-07 00:08:38 +0300

    fix TaskDetails

--- src/components/TaskDetails/Details/index.jsx
+++ src/components/TaskDetails/Details/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import DetailTabs from "../DetailTabs";
+import SubTasks from "../SubTasks";
+import "./Details.css";
 
 function Details() {
   return (
     <div className="details-container">
       <DetailTabs />
+      <div className="details-dashboard">
+        <div>
+          <SubTasks />
+          {/* <TimeGraph /> */}
+        </div>
+        <div className="">{/* Ortadaki Alan */}</div>
+        <div>{/* <AddTime /> */}</div>
+      </div>
     </div>
   );
 }
