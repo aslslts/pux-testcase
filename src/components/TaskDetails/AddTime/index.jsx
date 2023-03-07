1a2cbb1b8c34894dce90fb20ac39726323f17ba9
Aslı Sulutaş <cemree.1402@gmail.com>
2023-03-07 17:24:44 +0300

    add AddTime component

---
+++ src/components/TaskDetails/AddTime/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./AddTime.css";
+import TaskUserIcon from "../../../assets/task-icons/task-user.svg";
+import TaskDropDownIcon from "../../../assets/task-icons/task-dropdown.svg";
+import TaskCalendarIcon from "../../../assets/task-icons/task-calendar.svg";
+import TaskClockIcon from "../../../assets/task-icons/task-clock.svg";
+
+function AddTime() {
+  return (
+    <div className="add-time-container">
+      <div className="add-time-table">
+        <div className="add-time-text">Süre Ekle</div>
+        <div className="add-time-select-person">
+          <span className="add-time-main-text">Kim</span>
+          <div className="add-time-person-button">
+            <img src={TaskUserIcon} />
+            <span className="add-time-person-text">Holly Golightly</span>
+          </div>
+        </div>
+        <div>
+          <span className="add-time-main-text">Tarih</span>
+          <div className="add-time-calendar-button">
+            <img src={TaskCalendarIcon} />
+            <span className="add-time-select-text">Tarih seçiniz</span>
+            <img src={TaskDropDownIcon} />
+          </div>
+        </div>
+        <div className="add-time-start-finish">
+          <span className="add-time-main-text">Başlangıç Zamanı</span>
+          <div className="add-time-start-finish-button">
+            <img src={TaskClockIcon} />
+            <span className="add-time-calculate">09.10</span>
+          </div>
+          <span className="add-time-main-text">Bitiş Zamanı</span>
+          <div className="add-time-start-finish-button">
+            <img src={TaskClockIcon} />
+            <span className="add-time-calculate">09.50</span>
+          </div>
+        </div>
+        <div className="add-time-calculator">
+          <span className="add-time-main-text">Harcanan Süre</span>
+          <span className="add-time-calculate">0.50</span>
+        </div>
+        <div className="add-time-description">
+          <span className="add-time-main-text">Açıklama</span>
+          <div className="add-time-note-button">
+            <span className="add-time-note-text">Not Yazınız</span>
+          </div>
+        </div>
+        <div className="add-time-add-button">
+          <span className="add-time-add-text">Ekle</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTime;
