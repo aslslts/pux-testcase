8484e733475108c1735fad0290e08c7d0d440b21
Aslı Sulutaş <cemree.1402@gmail.com>
2023-03-07 17:41:37 +0300

    fix TimeGraph component

--- src/components/TaskDetails/TimeGraph/index.jsx
+++ src/components/TaskDetails/TimeGraph/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./TimeGraph.css";
+import TimeGraphPieIcon from "../../../assets/task-icons/graph-pie.svg";
 
 function TimeGraph() {
-  return <div>TimeGraph</div>;
+  return (
+    <div className="time-graph-container">
+      <div>
+        <span>Süre Grafiği</span>
+      </div>
+      <div>
+        <span>İlerleme Durumu</span>
+        <span>Toplam </span>
+        <span>24 sa</span>
+        <span>İlerleme </span>
+        <span>11 sa 32 dk</span>
+      </div>
+      <img src={TimeGraphPieIcon} />
+    </div>
+  );
 }
 
 export default TimeGraph;
