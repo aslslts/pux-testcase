05255df5ac75d7c48ffdab34fc3e6e192a461fed
Aslı Sulutaş <cemree.1402@gmail.com>
2023-03-08 14:50:05 +0300

    add TimeGraph css

--- src/components/TaskDetails/TimeGraph/index.jsx
+++ src/components/TaskDetails/TimeGraph/index.jsx
@@ -5,17 +5,23 @@ import TimeGraphPieIcon from "../../../assets/task-icons/graph-pie.svg";
 function TimeGraph() {
   return (
     <div className="time-graph-container">
-      <div>
-        <span>Süre Grafiği</span>
+      <div className="time-graph-main">
+        <span className="time-graph-text">Süre Grafiği</span>
       </div>
-      <div>
-        <span>İlerleme Durumu</span>
-        <span>Toplam </span>
-        <span>24 sa</span>
-        <span>İlerleme </span>
-        <span>11 sa 32 dk</span>
+      <div className="time-graph-ps-container">
+        <div className="time-graph-progress-status">
+          <span className="progress-status-text ps-text">İlerleme Durumu</span>
+          <div className="progress-status-total">
+            <span className="progress-status-text total-text ">Toplam </span>
+            <span className="progress-status-text hour-text">24 sa</span>
+          </div>
+          <div className="progress-status-progression">
+            <span className="progress-status-text total-text ">İlerleme </span>
+            <span className="progress-status-text hour-text">11 sa 32 dk</span>
+          </div>
+        </div>
+        <img className="time-graph-pie-icon" src={TimeGraphPieIcon} />
       </div>
-      <img src={TimeGraphPieIcon} />
     </div>
   );
 }
